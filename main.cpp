a8f54644ae182c59460b02843f3c0f437b77e24a
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-03-12 10:47:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,12 @@ using namespace std;
 int main(){
 float a=0, b=0, c=0, Quadrato, Rettangolo, Trapezio, Triangolo;
 
-cout << "Inserisci tre valori: " << endl;
 cin >> a >> b >> c;
-cout << "\n" << endl;
 
 Quadrato=a*a;
 Rettangolo=a*b;
 Trapezio=(((a*b)*c)/2);
 Triangolo=((a*b)/2);
-cout << "Le aree del Quadrato, Rettangolo, Trapezio e Triangolo sono rispettivamente: " << "\n" << Quadrato << "\n" << Rettangolo << "\n" << Trapezio << "\n" << Triangolo << endl;
+cout << Quadrato << "\n" << Rettangolo << "\n" << Trapezio << "\n" << Triangolo << endl;
 
 }
