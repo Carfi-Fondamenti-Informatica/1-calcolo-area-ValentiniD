dc8e6fb9413dee1c52c46245f10b847f326fd84f
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-03-12 11:03:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-float a=0, b=0, c=0, Quadrato, Rettangolo, Trapezio, Triangolo;
+float a=0, b=0, c=0, Area_Quadrato, Area_Rettangolo, Area_Trapezio, Area_Triangolo;
 
 cout << "Inserisci tre valori: " << endl;
 cin >> a >> b >> c;
 cout << "\n" << endl;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
-Quadrato=a*a;
-Rettangolo=a*b;
-Trapezio=(((a*b)*c)/2);
-Triangolo=((a*b)/2);
-cout << "Le aree del Quadrato, Rettangolo, Trapezio e Triangolo sono rispettivamente: " << "\n" << Quadrato << "\n" << Rettangolo << "\n" << Trapezio << "\n" << Triangolo << endl;
+Area_Quadrato=a*a;
+Area_Rettangolo=a*b;
+Area_Trapezio=(((a*b)*c)/2);
+Area_Triangolo=((a*b)/2);
+cout << "Le aree del Quadrato, Rettangolo, Trapezio e Triangolo sono rispettivamente: " << "\n" << Area_Quadrato << "\n" << Area_Rettangolo << "\n" << Area_Trapezio << "\n" << Area_Triangolo << endl;
 
 return 0
 
